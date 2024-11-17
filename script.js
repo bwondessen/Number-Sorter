0a8295262e03b0b9ba9ba616fd7d04d0c9985547
bwondessen <bwondessen23@gmail.com>
2024-11-16 22:31:39 -0600

    Create updateUI function and iterate over array

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+
+    const inputValues = [
+        ...document.getElementsByClassName("values-dropdown")
+    ].map((dropdown) => Number(dropdown.value));
+}
+
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+    })
 }
 
 sortButton.addEventListener("click", sortInputArray);
